d7f447e74d8222907f775fca6cbd01897866f114
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2017-11-26 20:59:11 -0800

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,18 @@
 console.log("Up and running!");
 
-var cardOne = "queen" ;
-var cardTwo = "king" ;
-var cardThree = "queen" ;
-var cardFour = "king" ;
+var cards = ["queen", "queen", "king", "king"] ;
+var cardsInPlay = [] ;
+
+var cardOne = cards[0] ;
+cardsInPlay.push(cardOne) ;
+
+var cardTwo = cards[2] ;
+cardsInPlay.push(cardTwo) ;
+
+if (cardsInPlay.length === 2) {
+	console.log("2 cards") 
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
