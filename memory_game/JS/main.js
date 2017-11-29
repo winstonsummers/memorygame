7e6c12fc4f6886aa072a47dad150e099f908a3c9
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2017-11-28 22:26:04 -0800

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -3,16 +3,30 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"] ;
 var cardsInPlay = [] ;
 
-var cardOne = cards[0] ;
-cardsInPlay.push(cardOne) ;
-
-var cardTwo = cards[2] ;
-cardsInPlay.push(cardTwo) ;
-
-if (cardsInPlay.length === 2) {
-	console.log("2 cards") 
-} else if (cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId])
+
+	cardsInPlay.push(cards[cardId])
+
+	
+	
+	if (cardsInPlay.length === 2) {
+		console.log("2 cards") 
+		checkForMatch()
+	// } else if (cardsInPlay[0] === cardsInPlay[2]) {
+	// 	alert("You found a match!")
+	// } else {
+	// 	alert("Sorry, try again.")
+	}
+}
+
+flipCard(0)
+flipCard(2)
